d84ec1d8597df7f0ea8c079b5868665544ac4491
Hemant Choudhary <hemant75583@gmail.com>
2026-01-03 08:02:57 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".project-card.animate");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  cards.forEach((card) => observer.observe(card));
+});
